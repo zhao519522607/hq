3aa270b93dd808f6c4af96aaab4a918279b50e7b
Figure <zhao519522607@users.noreply.github.com>
2017-04-20 10:39:51 +0800

    Update codis_ops.py
    加入报错重试机制。

--- codis_ops.py
+++ codis_ops.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import zookeeper,redis
+import time
 
 ser_list = []
 ops = 0
-handler = zookeeper.init("1.1.1.1:2181")
-data = zookeeper.get_children(handler,"/zk/codis/db_hq_001/servers")
-for i in data:
-   i_list = zookeeper.get_children(handler,"/zk/codis/db_hq_001/servers/%s" %i)[0]
-   ser_list.append(i_list)
-zookeeper.close(handler)
+def codis_ops():
+        handler = zookeeper.init("1.1.1.1:2181")
+        data = zookeeper.get_children(handler,"/zk/codis/db/servers")
+        for i in data:
+                i_list = zookeeper.get_children(handler,"/zk/codis/db/servers/%s" %i)[0]
+                ser_list.append(i_list)
+        zookeeper.close(handler)
 
-for s_list in ser_list:
-   tmp = s_list.split(':')
-   r = redis.Redis(host=tmp[0], port=int(tmp[1]))
-   ops += r.info()["instantaneous_ops_per_sec"]
-with open('/tmp/codis_ops','w+') as f:
-     f.write(str(ops))
+if __name__ == '__main__':
+        try:
+                codis_ops()
+        except:
+                time.sleep(2)
+                codis_ops()
+
+        for s_list in ser_list:
+                tmp = s_list.split(':')
+                r = redis.Redis(host=tmp[0], port=int(tmp[1]))
+                ops += r.info()["instantaneous_ops_per_sec"]
+        with open('/tmp/codis_ops','w+') as f:
+                f.write(str(ops))
