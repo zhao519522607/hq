df1698cdcbf531d28aaddd6f581bc29cd21ea183
Figure <zhao519522607@users.noreply.github.com>
2017-09-05 15:59:42 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,33 +1,43 @@
 #! /bin/bash
+PROJECT="与jenkins保持一致"
+LN_name=" "
 
 update() {
-	#保留最近3次的更新目录
-	ls -lt /data1/web/ |tail -n +2 |awk 'BEGIN {FS=" "} NR > 3 {print $NF}' |xargs -i rm -rf /data1/web/{}
-	#解压
-	tar zxf /data1/upload/hqs_test.tgz -C /data1/web/
-	name=hqs_test-$(date +"%F-%H%M")
-	mv /data1/web/hqs_test /data1/web/$name
-	if [ -h "/www/hqs_test" ];then
-		rm -rf /www/hqs_test
-		ln -s /data1/web/$name /www/hqs_test
-	fi
+        #保留最近3次的更新目录
+        ls -lt /mnt/web/$PROJECT |tail -n +2 |awk 'BEGIN {FS=" "} NR > 3 {print $NF}' |xargs -i rm -rf /mnt/web/$PROJECT/{}
+        #解压
+        tar zxf /mnt/upload/$PROJECT.tgz -C /mnt/web/$PROJECT/
+        name=$PROJECT-$(date +"%F-%H%M")
+        find /mnt/web/$PROJECT/$PROJECT -type d -name ".svn" -exec rm -rf {} \; > /dev/null 2>&1
+        mv /mnt/web/$PROJECT/$PROJECT /mnt/web/$PROJECT/$name
+        if [ -h "/mnt/www/$LN_name" ];then
+                rm -rf /mnt/www/$LN_name
+                ln -s /mnt/web/$PROJECT/$name /mnt/www/$LN_name
+		ln -s /mnt/www/voucher /mnt/web/$PROJECT/$name/voucher
+        else
+                ln -s /mnt/web/$PROJECT/$name /mnt/www/$LN_name
+		ln -s /mnt/www/voucher /mnt/web/$PROJECT/$name/voucher
+        fi
+	sudo /usr/bin/kill -USR2 $(cat /var/run/php-fpm/php-fpm.pid)
 }
 
 rollback() {
-	dir_ver=`ls -lt /data1/web/ |tail -n +2 |awk -v arg=$roll_v 'BEGIN {FS=" "} NR==arg {print $NF}'`
-	if [ -h "/www/hqs_test" ];then
-		rm -rf /www/hqs_test
-		ln -s /data1/web/$dir_ver /www/hqs_test
-	fi
-
+        dir_ver=`ls -lt /mnt/web/$PROJECT |tail -n +2 |awk -v arg=$roll_v 'BEGIN {FS=" "} NR==arg {print $NF}'`
+        if [ -h "/mnt/www/$LN_name" ];then
+                rm -rf /mnt/www/$LN_name
+                ln -s /mnt/web/$PROJECT/$dir_ver /mnt/www/$LN_name
+        else
+                ln -s /mnt/web/$PROJECT/$dir_ver /mnt/www/$LN_name
+        fi
+	sudo /usr/bin/kill -USR2 $(cat /var/run/php-fpm/php-fpm.pid)
 }
 
 case $1 in
-	update)
-		update
-		;;
-	rollback)
-		roll_v=$2
-		rollback
-		;;
+        update)
+                update
+                ;;
+        rollback)
+                roll_v=$2
+                rollback
+                ;;
 esac
