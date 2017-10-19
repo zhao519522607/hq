16c266ae2b0c424e4c23aadefe1dc7a3c4d7da35
Figure <zhao519522607@users.noreply.github.com>
2017-10-19 09:12:11 +0800

    Update no_commit.py
    修改提交限制脚本

--- no_commit.py
+++ no_commit.py
@@ -4,10 +4,20 @@ import sys
 import re
 import os
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 def get_file_list(repo, TXN):  
-        """svnlook changed -r REV REPOS 获得发生变更的文件 
+        """svnlook changed -t TXN REPOS 获得发生变更的文件 
         """  
         cmd = '%s changed -t %s %s' % (svnlook_bin_path, TXN, repo)  
+        output = os.popen(cmd).read()  
+        return output
+
+def get_log(repo, TXN):  
+        """svnlook log -t TXN REPOS 获得提交日志 
+        """  
+        cmd = '%s log -t %s %s' % (svnlook_bin_path, TXN, repo)  
         output = os.popen(cmd).read()  
         return output
 
@@ -19,10 +29,13 @@ def diff_dir(f_list,my_contxt):
 	else:
 		return False
 
-if __name__ == '__main__':  
-        svnlook_bin_path = '/usr/bin/svnlook'
-	file_list = get_file_list(sys.argv[1], sys.argv[2])
-	if diff_dir(file_list,'offline/devops'):
-		sys.exit(10)
-	else:
-		pass
+if __name__ == '__main__':
+	svnlook_bin_path = '/usr/bin/svnlook'
+	#file_list = get_file_list(sys.argv[1], sys.argv[2])
+	#if diff_dir(file_list,'offline/devops'):
+	#	sys.exit(10)
+	#else:
+	#	pass
+	commit_log = get_log(sys.argv[1], sys.argv[2])
+	with open('/data1/develop/svn_log', 'a+') as f:
+		f.write(commit_log)
